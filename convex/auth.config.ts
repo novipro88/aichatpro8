b6e95714c823c6989d26538965403fb1df20da08
novipro88 <noviantipali97@gmail.com>
2024-04-06 04:41:40 +0700

    auth & webhooks

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://busy-hermit-35.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
